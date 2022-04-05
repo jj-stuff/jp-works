58b1ffbbca37f6836b846f69728726d689c2a209
JJ <Stephanjdai@gmail.com>
2022-04-06 01:19:57 +0200

    Fix navbar

--- index.php
+++ index.php
@@ -9,15 +9,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/wall.css">
 </head>
 
 <body>
+    <div class="d-flex flex-column">
+
     <?php include 'navbar.html'?>
     <div class="main">
-
+        <section class="row" style="background-image: url(img/moon1920.jpg);">
+            <div class="col-8">
+            </div>
+            <div class="col-4">
+                <img src="img/proud.jpeg" alt="">
+            </div>
+        </section>
         <section class="row bg-black">
-            <div class="col-8" id="RecentW">
+            <div class="col-8 pt-5">
                 <h2>Recent Work</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium et vel commodi dolorum beatae
                     earum
@@ -32,17 +39,14 @@
             <div class="col-4">
                 <img src="img/proud.jpeg" alt="">
             </div>
-            <div class="col-8 right-text">
+            <div class="col-8 text-right">
                 <h1>WOW somehting here</h1>
             </div>
         </section>
-        <section class="row" style="background-image: url(img/moon1920.jpg);">
-            <div class="col-12">
-                <h3>Cookies</h3>
-            </div>
-        </section>
     </div>
+
     <?php include 'footer.html' ?>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
