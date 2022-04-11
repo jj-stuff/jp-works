b30979313c7a64915fb3eea2b6e815d79f8006fc
JJ <Stephanjdai@gmail.com>
2022-04-11 19:52:32 +0200

    TRANSFER

--- index.php
+++ index.php
@@ -13,42 +13,52 @@
 
 <body>
     <div class="d-flex flex-column">
-    <?php include 'navbar.html'?>
-    <div>
-        <section class="row" style="background-image: url(img/moon1920.jpg);">
-            <div class="col-8">
-            </div>
-            <div class="col-4">
-                <img src="img/proud.jpeg" alt="">
-            </div>
-        </section>
-        <section class="row bg-black">
-            <div class="col-8 pt-5">
-                <h2>Recent Work</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium et vel commodi dolorum beatae
-                    earum
-                    odio laborum eaque at, quae soluta a mollitia molestias quia quisquam. Rerum nihil molestiae dolore?
-                </p>
-            </div>
-            <div class="col-4 p-5">
-                <img src="img/kaguyasama_s3.jpg" height="500px">
-            </div>
-        </section>
-        <section class="row bg-black bg-opacity-75">
-            <div class="col-4">
-                <img src="img/proud.jpeg" alt="">
-            </div>
-            <div class="col-8 text-right">
-                <h1>WOW somehting here</h1>
-            </div>
-        </section>
-    </div>
+        <?php include 'navbar.html'?>
+        <div>
+            <section class="row" style="background-image: url(img/moon1920.jpg);">
+                <div class="col-8">
+                </div>
+                <div class="col-4">
+                    <img src="img/proud.jpeg" alt="">
+                </div>
+            </section>
+            <section class="row bg-black">
+                <div class="col-8 pt-5">
+                    <h1>Airing</h1>
+                    <h5>Kaguya-sama: Love Is War: Ultra Romantic</h5>
+                    <p class="text-white-50">
+                        The third season of Kaguya-sama: Love Is War, titled
+                        , Kaguya-sama: Love Is War: Ultra Romantic, is a 2022 Japanese anime series, based
+                        on the manga
+                        series of the same title, written and illustrated by. It was announced on October 25, 2020 for
+                        production
+                        during the "Kaguya-sama Wants To Tell You On Stage" special event. The season premiered on April
+                        9, 2022, with returning staff and cast members. The opening theme is "Giri Giri" by Masayuki
+                        Suzuki
+                        featuring Suu from Silent Siren, while the ending theme is
+                        "Heart wa Oteage" ("My Heart Does Not Know What to Do") by Airi Suzuki.
+                    </p>
+                </div>
+                <div class="col-4 p-5">
+                    <img src="img/kaguyasama_s3.jpg" height="500px">
+                </div>
+            </section>
+            <section class="row bg-black bg-opacity-75">
+                <div class="col-4">
+                    <img src="img/proud.jpeg" alt="">
+                </div>
+                <div class="col-8 text-right">
+                    <h1>WOW somehting here</h1>
+                </div>
+            </section>
+        </div>
 
-    <?php include 'footer.html' ?>
+        <?php include 'footer.html' ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
     <script src="js/script.js"></script>
 </body>
+
 </html>
