b5c2fb81bcdb0b42310e8eefbdbea7d0f7cc42d8
JJ-Pr0 <Stephanjdai@gmail.com>
2022-04-11 20:58:22 +0200

    active php

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<nav class="nav bg-black bg-opacity-50 blur">
+    <div class="container">
+        <div class="row p-3">
+            <a href="" class="col-1">
+                    <img src="img/A-1_White.svg" id='logo' alt="logo">
+            </a>
+            <ul class="nav-links my-auto d-flex col-11">
+                <li class="nav-item <?php echo $active['index']?>"><a href="index" class="active nav-link">Home</a></li>
+                <li class="nav-item <?php echo $active['work']?>"><a href="work" class="nav-link">Works</a></li>
+                <li class="nav-item <?php echo $active['about']?> ms-auto"><a href="" class="nav-link">About Me</a></li>
+            </ul>
+        </div>
+</nav>
