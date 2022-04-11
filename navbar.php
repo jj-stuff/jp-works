535ea9e68bbf9823ea1ed6dba139d38fdb73a001
JJ-Pr0 <Stephanjdai@gmail.com>
2022-04-11 23:29:52 +0200

    logo fix

--- navbar.php
+++ navbar.php
@@ -12,10 +12,10 @@
 <nav class="nav bg-black bg-opacity-50 blur">
     <div class="container">
         <div class="row p-3">
-            <a href="" class="col-1">
+            <a href="" class="col-sm-2 col-md-1">
                     <img src="img/A-1_White.svg" id='logo' alt="logo" class="navbar-brand d-none d-sm-inline mb-2">
             </a>
-            <ul class="nav-links my-auto d-flex col-11">
+            <ul class="nav-links my-auto d-flex col-sm-10 col-md-11">
                 <li class="nav-item "><a href="index" class="nav-link <?php echo $active['index']?>">Home</a></li>
                 <li class="nav-item"><a href="work" class="nav-link <?php echo $active['work']?>">Works</a></li>
                 <li class="nav-item ms-auto"><a href="about" class="nav-link <?php echo $active['about']?>">About Me</a></li>
