5199be287b9bb53858f6587fb2405c23a19e6566
JJ <Stephanjdai@gmail.com>
2022-04-25 12:05:09 +0200

    Navbar Fixed sus

--- navbar.php
+++ navbar.php
@@ -1,15 +1,8 @@
 <?php 
 
-// -Stephan NOTE NO IDEA What's going on here lol
-
     // gets the current URI, remove the left / and then everything after the / on the right
-    $directory = explode('/',ltrim($_SERVER['REQUEST_URI'],'/'));
+    $url = '//' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
-    // loop through each directory, check against the known directories, and add class   
-    $directories = array("index", "work","about"); // set home as 'index', but can be changed based of the home uri
-    foreach ($directories as $folder){
-    $active[$folder] = ($directory[0] == $folder)? "on":"noactive";
-    }
 ?>
 
 <nav class="nav bg-black bg-opacity-50 blur">
@@ -19,9 +12,9 @@
                     <img src="img/A-1_White.svg" id='logo' alt="logo" class="navbar-brand d-none d-sm-inline m-0">
             </a>
             <ul class="nav-links my-auto d-flex col-sm-10 col-md-11">
-                <li class="nav-item "><a href="index" class="nav-link <?php echo $active['index']?>">Home</a></li>
-                <li class="nav-item"><a href="work" class="nav-link <?php echo $active['work']?>">Works</a></li>
-                <li class="nav-item ms-auto"><a href="about" class="nav-link <?php echo $active['about']?>">About Me</a></li>
+                <li class="nav-item "><a href="index" class="nav-link <?php if (strpos($url,'index') == true) { echo ' on'; } ?>">Home</a></li>
+                <li class="nav-item"><a href="work" class="nav-link <?php if (strpos($url,'work') !== false) { echo ' on'; } ?>">Works</a></li>
+                <li class="nav-item ms-auto"><a href="about" class="nav-link <?php if (strpos($url,'about') == true) { echo ' on'; } ?>">About Me</a></li>
             </ul>
         </div>
 </nav>
