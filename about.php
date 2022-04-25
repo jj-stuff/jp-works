398baf514400a4e4195da9b784a306d8f3d422d0
JJ <Stephanjdai@gmail.com>
2022-04-25 10:53:04 +0200

    JUAN

--- about.php
+++ about.php
@@ -16,82 +16,15 @@
     <div class="overflow-hidden">
         <?php include 'navbar.php'?>
         <main>
-            <div id="particles-js"></div>
-            <section class="row" id='sao'>
-                <div class="col-sm-12 col-md-6 px-5">
-                    <div id="sliderShow" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                        <div class="carousel-inner mx-auto">
-                            <div class="carousel-item active">
-                                <img src="img/progressive.png" alt="cover">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img/progressive2.png" alt="cover">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img/progressive3.png" alt="cover">
-                            </div>
-                        </div>
+            <section>
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <img src="https://i.kym-cdn.com/entries/icons/facebook/000/035/644/juancover.jpg" alt="" class="">
                     </div>
                 </div>
-
-                <div class="col-6 text-right px-5 d-none d-md-block position-relative">
-                    <img src="img/progressive_logo.png" width="115%">
-                    <p class="text-grey">
-                        This is the story of a time before Lightning Flash and the Black Swordsman were known by those
-                        names…
-                        <br>
-                        <br>
-                        Asuna Yuuki was a girl in the ninth grade who'd never had anything to do with online games when
-                        she accidentally put on the NerveGear that fateful day.
-                        <br>
-                        <br>
-                        The game master speaks.
-                        This may be a game, but it's not something you play.
-                        “Game Over” means you die in the real world.
-                        <br>
-                        <br>
-                        Hearing this, the players are sent into a state of confusion, and pandemonium reigns in the
-                        game. One of these players is Asuna. Despite not even knowing the rules of this new world, she
-                        sets out on her journey to the floating steel castle of Aincrad, its apex not yet visible.
-                        <br>
-                        <br>
-                        As she continues to live on in this world where death is always a step away, Asuna has a fateful
-                        encounter followed by a parting…
-
-                    </p>
-                    <a href="https://anilist.co/anime/124140/Sword-Art-Online-Progressive--Hoshinaki-Yoru-no-Aria/"
-                        target="_blank"><img src="img/anilist.svg" class="btn-right" width="120px"></a>
-                </div>
             </section>
-
-            <section class="row bg-black" id='kaguya'>
-                <div class="col-7 p-5 d-none d-md-block position-relative">
-                    <h1>Airing</h1>
-                    <h5>Kaguya-sama: Love Is War: Ultra Romantic</h5>
-                    <p class="text-white-50">
-                        The third season of Kaguya-sama: Love Is War, titled
-                        , Kaguya-sama: Love Is War: Ultra Romantic, is a 2022 Japanese anime series, based
-                        on the manga
-                        series of the same title, written and illustrated by. It was announced on October 25, 2020
-                        for
-                        production
-                        during the "Kaguya-sama Wants To Tell You On Stage" special event. The season premiered on
-                        April
-                        9, 2022, with returning staff and cast members. The opening theme is "Giri Giri" by Masayuki
-                        Suzuki
-                        featuring Suu from Silent Siren, while the ending theme is
-                        "Heart wa Oteage" ("My Heart Does Not Know What to Do") by Airi Suzuki.
-                    </p>
-                    <a href="https://anilist.co/anime/125367/Kaguyasama-wa-Kokurasetai-Ultra-Romantic/"
-                        target="_blank"><img src="img/anilist.svg" class="btn-left" width="120px"></a>
-                </div>
-                <div class="col-sm-12 col-md-5">
-                    <img src="img/kaguyasama_s3.jpg" class="image float-end">
-                </div>
-            </section>
-
         </main>
-        <?php include 'footer.html'?>
+        <?php include 'footer.php'?>
     </div>
 
     <script src="js/particles.js"></script>
